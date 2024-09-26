6b35fcfe4ec3bf7f10c6b7ad6c604ac9bde4b5d9
sma0729mex <sma0729mex@gmail.com>
2024-09-26 13:16:32 +0000

    微調整

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,24 @@
 import { Component, type OnInit, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PrimeNGConfig } from 'primeng/api';
-import { Aura } from 'primeng/themes/aura';
 import { ButtonModule } from 'primeng/button';
+import { Aura } from 'primeng/themes/aura';
 
 @Component({
 	selector: 'app-root',
 	standalone: true,
-	imports: [RouterOutlet,ButtonModule],
+	imports: [RouterOutlet, ButtonModule],
 	templateUrl: './app.component.html',
 	styleUrl: './app.component.css',
 })
 export class AppComponent implements OnInit {
-  config = inject(PrimeNGConfig);
+	config = inject(PrimeNGConfig);
 
-  constructor(){
-    this.config.theme.set({preset:Aura});
-  }
+	constructor() {
+		this.config.theme.set({ preset: Aura });
+	}
 
-  ngOnInit(){
-    this.config.ripple.set(true);
-  }
+	ngOnInit() {
+		this.config.ripple.set(true);
+	}
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,7 @@
-import { type ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+	type ApplicationConfig,
+	provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
